9b04298cc31b11ea153864c1695145f3a93a0757
medical-boy-sf <medical_boy_sf@yahoo.com>
2019-01-07 15:01:30 +0200

    03. Blood sugar measurement

---
+++ 03.BloodSugarMeasurement/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _03.BloodSugarMeasurement
+{
+    class Program
+    {
+        static void Main()
+        {
+            double bloodSugarLevel = double.Parse(Console.ReadLine());
+            string units = Console.ReadLine();
+
+            if (units == "mg/dl")
+            {
+                if (true)
+                {
+
+                }
+            }
+
+            else if (units == "mmol/l")
+            {
+
+            }
+
+            else
+            {
+                Console.WriteLine("Unrecognized unit. Check if you have spelled correctly - 'mg/dl' or 'mmol/l'");
+            }
+
+
+        }
+    }
+}
