35cda68f5da96ded31e3d5a6b71a2cdde455bc2a
medical-boy-sf <medical_boy_sf@yahoo.com>
2019-01-07 11:07:02 +0200

    test

---
+++ 01.DrawingTimeBar/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace _01.DrawingTimeBar
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.CursorVisible = false;
+            int counter = 1;
+            while (true)
+            {
+                if (counter > 10)
+                {
+                    counter = 1;
+                    Console.Clear();
+                }
+                Console.Write(counter + " ");
+                Thread.Sleep(1500);
+                counter += 1;
+            }
+        }
+    }
+}
---
+++ 02.LatinOrCyrillic/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _02.LatinOrCyrillic
+{
+    class Program
+    {
+        static void Main()
+        {
+            string word = Console.ReadLine();
+
+            if ((word[0] >= 'A' && word[0] <= 'Z') || (word[0] >= 'a' && word[0] <= 'z'))
+            {
+                Console.WriteLine("Latin");
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if ((word[i] >= 'а' && word[i] <= 'я') || (word[i] >= 'А' && word[i] <= 'Я'))
+                    {
+                        Console.WriteLine("There is Cyrillic letters also!");
+                    }
+                }
+            }
+
+            else if ((word[0] >= 'а' && word[0] <= 'я') || ((word[0] >= 'А' && word[0] <= 'Я')))
+            {
+                Console.WriteLine("Cyrillic");
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if ((word[i] >= 'A' && word[i] <= 'Z') || (word[i] >= 'a' && word[i] <= 'z'))
+                    {
+                        Console.WriteLine("There is Latin letters also!");
+                    }
+                }
+            }
+        }
+    }
+}
