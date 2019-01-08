626d992273e28833355fe3d21dfbb50e3f7a4d30
medical-boy-sf <medical_boy_sf@yahoo.com>
2019-01-09 01:37:40 +0200

    04.BodyMassIndex

---
+++ 04.BodyMassIndex/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _04.BodyMassIndex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+					Console.Write("Your height in meters: ");
+					double height = double.Parse(Console.ReadLine());
+					Console.Write("Your weight in kilograms: ");
+					double weight = double.Parse(Console.ReadLine());
+					double bmi = weight / (height * height);
+					
+					if (bmi > 25) 
+					{
+						Console.WriteLine($"Your BMI is {bmi:F2}"); 
+						Console.WriteLine("Overweight!");
+					}
+
+					else if (bmi < 18)
+					{
+            Console.WriteLine($"Your BMI is {bmi:F2}");
+						Console.WriteLine("Underweight!");
+					}
+
+					else 
+					{
+						Console.WriteLine($"Your BMI is {bmi:F2}");
+						Console.WriteLine("You have normal weight");
+					}
+
+        }
+    }
+}
