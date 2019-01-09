c56e58de76de4368be2af4cdb3ff5c9ffa327741
medical-boy-sf <medical_boy_sf@yahoo.com>
2019-01-09 20:21:18 +0200

    DayOrNight

---
+++ DayOrNight/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DayOrNight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var date = DateTime.Now;
+            int hour = date.Hour;
+            Console.WriteLine(hour);
+
+            if ((hour >= 0 && hour < 5) || (hour >= 18 && hour < 24))
+            {
+                Console.WriteLine("Night");
+            }
+
+            else
+            {
+                Console.WriteLine("Day");
+            }
+        }
+    }
+}
